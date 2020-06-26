055a851d4d3b3685b51b0d1ce1668f4e8e5bdc2e
Marcelo Lazaroni <lazaronijunior@gmail.com>
2020-06-26 19:52:03 +0100

    Add executable

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,17 @@
 module Main where
 
 main = print "Hello"
+    {-
+data Expr a
+    = Const a
+    | Cond (Expr Bool) (Expr a) (Expr a)
+    | Fun ,2
+
+
+
+ex = do
+    if exists "g:loaded_whid"
+       then undefined
+       else undefined
+
+    -}
