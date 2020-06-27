25a3651868dcb2ba83867909ccc7bf9a2200c539
Marcelo Lazaroni <lazaronijunior@gmail.com>
2020-06-27 14:51:49 +0100

    Create shell with Cabal 3

--- shell.nix
+++ shell.nix
@@ -1,4 +1,21 @@
+{ sources ? import ./nix/sources.nix
+}:
+with { overlay = _: pkgs: { niv = import sources.niv {}; }; };
 let
+    pkgs = import sources.nixpkgs {};
     vimscript-haskell = import ./default.nix {};
 in
-   vimscript-haskell.env
+  pkgs.haskellPackages.shellFor {
+    packages = pkgs: [
+      vimscript-haskell
+    ];
+
+    # withHoogle = true;
+    buildInputs = with pkgs.haskellPackages; [
+      cabal-install #  Make sure we are using cabal version 3
+    ];
+
+    # Prevents cabal from choosing alternate plans, so that
+    # *all* dependencies are provided by Nix.
+    exactDeps = true;
+  }
