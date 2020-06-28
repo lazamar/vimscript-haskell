91ecc8c88d3cc3331f7549c2b1342398588adb5b
Marcelo Lazaroni <lazaronijunior@gmail.com>
2020-06-29 00:44:45 +0100

    Add watch commands to shell.nix

--- shell.nix
+++ shell.nix
@@ -4,6 +4,17 @@ with { overlay = _: pkgs: { niv = import sources.niv {}; }; };
 let
     pkgs = import sources.nixpkgs {};
     vimscript-haskell = import ./default.nix {};
+
+    # Commands
+    commands = {
+      test-watch = pkgs.writeShellScriptBin "test-watch" ''
+        ls src/*.hs test/*.hs | entr -dsc 'cabal build && cabal run vimscript-test'
+        '';
+
+      run-watch = pkgs.writeShellScriptBin "run-watch" ''
+        ls src/*.hs | entr -dsc 'cabal build && cabal run vimscript-haskell-exe'
+        '';
+    };
 in
   pkgs.haskellPackages.shellFor {
     packages = pkgs: [
@@ -17,6 +28,8 @@ in
       pkgs.haskell.packages.ghc882.ghcide
       pkgs.entr
       pkgs.vim
+      commands.test-watch
+      commands.run-watch
     ];
 
     # Prevents cabal from choosing alternate plans, so that
--- test.sh
+++
@@ -1,2 +0,0 @@
-nix-shell --pure --command \
-    "ls src/*.hs test/*.hs | entr  -dsc 'cabal build vimscript-test && cabal run vimscript-test'"
