6b3fb7d397c4c9b480ff17bdf8e8e6ae4b37e6ac
Marcelo Lazaroni <lazaronijunior@gmail.com>
2020-07-05 00:29:55 +0100

    Enable string testing

--- test/ExpressionSpec.hs
+++ test/ExpressionSpec.hs
@@ -8,30 +8,55 @@ module ExpressionSpec (spec) where
 import Vim.Expression
 import Text.Read (readMaybe)
 import Test.Hspec.Expectations (HasCallStack)
-import Test.Hspec (Spec, describe, it)
+import Test.Hspec (Spec, describe, it, parallel)
 import Prelude hiding (Eq, True, False)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Except (runExceptT)
+import Test.QuickCheck (property, Arbitrary(..), suchThat)
+import Data.Maybe (fromMaybe)
+import Data.String (fromString)
+import Data.Bifunctor (first)
+import System.Timeout (timeout)
 
 import qualified Prelude as P
 import qualified Test.Hspec as Hspec
 import qualified Vim.Run as Vim
 
 spec :: Spec
-spec = do
+spec = parallel $ do
     describe "Primary types" $ do
-        it "number" $ shouldEqual 1 $ return (1:: Expr Int)
+        describe "Number" $ do
+            it "can be generated from any Haskell number"
+                $ property $ \number -> shouldEqual number $ return (fromIntegral number :: Expr Int)
+
+        describe "String" $ do
+            it "can be generated from any Haskell String"
+                  $ property $ \(NonEmptyString string) ->
+                      shouldEqual string $ return (fromString string :: Expr String)
+
         it "string" $ shouldEqual "hi" $ return ("hi" :: Expr String)
-        it "string with escaped characters" $ shouldEqual "hi" $ return ("\"hi\"" :: Expr String)
+        it "string with escaped characters" $ shouldEqual "hi" $ return ("\"\''hi" :: Expr String)
         it "boolean False" $ shouldEqual P.False $ return false
         it "boolean True" $ shouldEqual P.True $ return true
 
 shouldEqual :: forall a. (Show a, P.Eq a, VimRead a, HasCallStack)
     => a -> Vim (Expr a) -> IO ()
 shouldEqual val program = do
-    eitherRes <- runExceptT $ Vim.getResult program
-    let res = vimRead $ either (error . show) id eitherRes
+    eitherRes <- withTimeout (100 * millisecond) $ runResult $ Vim.getResult program
+    let res = vimRead $ either error id eitherRes
     Right val `Hspec.shouldBe` res
+    where
+        runResult = fmap (first Vim.showExecError) . runExceptT
+        withTimeout t
+            = fmap (fromMaybe (Left "Timeout"))
+            . timeout (t * millisecond)
+        millisecond = 1000
+
+newtype NonEmptyString = NonEmptyString String
+    deriving (Show)
+
+instance Arbitrary NonEmptyString where
+    arbitrary = fmap NonEmptyString $ arbitrary `suchThat` (P./= "")
 
 class VimRead a where
     vimRead :: String -> Either String a
